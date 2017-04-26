0980927b93f75ecf29518e2f28034350f865ee20
l4cuatro <lpieroni@sfhighschool.net>
2017-04-26 14:14:25 -0700

    Fixed a thing

--- scoutDb/teamStatTracker.hpp
+++ scoutDb/teamStatTracker.hpp
@@ -19,66 +19,68 @@ limitations under the License.
 
 using namespace std;
 
-public class Team {
-	
-	unsigned short int teamIndex;
-	
-	unsigned short int allies[15],
-		opponents[30];
-		
-	unsigned char record[3];
-	
-	unsigned char ap,
-		wp;
-	
-	int sp;
-	
-	int allyScoreSum,
-		oppScoreSum;
-	
-	string number; //Team Number
-	
-	Team(string num, unsigned short int index) { //Constructor
-		teamIndex = index;
-		number = num;
-		sp = 0;
-		ap = 0;
-		allyScoreSum = 0;
-		oppScoreSum = 0;
-		for(int i = 0; i < sizeof(record); i++)
-			record[i] = 0;
-		
-		for(int i = 0; i < sizeof(allies) / sizeof(short); i++) {
-			allies[i] = 0;
-			opponents[2 * i] = 0;
-			opponents[2 * i + 1] = 0;
-		}
-	}
+namespace ScoutDb {
 
-	void upStats(unsigned short int ally, unsigned short int opponent1, unsigned short int opponent2, bool bDidWinAuton, short int score, short int oppScore, unsigned char matchInd) {
-		allies[matchInd] = ally;
-		opponents[2 * matchInd] = opponent1;
-		opponents[2 * matchInd + 1] = opponent2;
-		if(score > oppScore) { //If a win
-			record[0]++;
-			sp += oppScore;
+	public class Team {
+		
+		unsigned short int teamIndex;
+		
+		unsigned short int allies[15],
+			opponents[30];
+			
+		unsigned char record[3];
+		
+		unsigned char ap,
+			wp;
+		
+		int sp;
+		
+		int allyScoreSum,
+			oppScoreSum;
+		
+		string number; //Team Number
+		
+		Team(string num, unsigned short int index) { //Constructor
+			teamIndex = index;
+			number = num;
+			sp = 0;
+			ap = 0;
+			allyScoreSum = 0;
+			oppScoreSum = 0;
+			for(int i = 0; i < sizeof(record); i++)
+				record[i] = 0;
+			
+			for(int i = 0; i < sizeof(allies) / sizeof(short); i++) {
+				allies[i] = 0;
+				opponents[2 * i] = 0;
+				opponents[2 * i + 1] = 0;
+			}
 		}
-		else if(score < oppScore) { //If a loss
-			record[1]++;
-			sp+= score;
+
+		void upStats(unsigned short int ally, unsigned short int opponent1, unsigned short int opponent2, bool bDidWinAuton, short int score, short int oppScore, unsigned char matchInd) {
+			allies[matchInd] = ally;
+			opponents[2 * matchInd] = opponent1;
+			opponents[2 * matchInd + 1] = opponent2;
+			if(score > oppScore) { //If a win
+				record[0]++;
+				sp += oppScore;
+			}
+			else if(score < oppScore) { //If a loss
+				record[1]++;
+				sp += score;
+			}
+			else { //If a tie
+				record[2]++;
+				sp += score;
+			}
+			allyScoreSum += score;
+			oppScoreSum += oppScore;
+			if(bDidWinAuton)
+				ap++;
+			wp = (2 * record[0]) + record[2];
+			return;
 		}
-		else { //If a tie
-			record[2]++;
-			sp+= score;
-		}
-		allyScoreSum += score;
-		oppScoreSum += oppScore;
-		if(bDidWinAuton)
-			ap++;
-		wp = 2 * record[0] + record[2];
-		return;
+	
 	}
-	
-	
 	
 }
