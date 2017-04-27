440b4e0d04d38ed6fe3ed2210065bf5fc5d5b8ad
l4cuatro <lpieroni@sfhighschool.net>
2017-04-27 14:42:36 -0700

    Updated a few things, let's see if this works

--- scoutDb/matMath.hpp
+++ scoutDb/matMath.hpp
@@ -6,23 +6,35 @@ namespace MatrixMath {
 		
 		int matDims;
 		
-		MatrixInverter(int matSize) {
+		MatrixInverter(int* mat, int matSize) {
 			
 			matDims = matSize;
 			
-			int* arr[matSize];
+			int* arr[matSize],
+				identMat[matSize];
 			
-			for(int i = 0; i < matSize; i++)
+			for(int i = 0; i < matSize; i++) {
 				arr[i] = (int*)(malloc(matSize * sizeof(int));
+				identMat[i] = (int*)(malloc(matSize * sizeof(int));
+			}
+			
+			for(int i = 0; i < matSize; i++) {
+				for(int j = 0; j < matSize; j++) {
+					arr[i][j] = mat[i][j];
+					identMat[i][j] = 0;
+				}
+				identMat[i][i] = 1;
+			}
+			
 		}
 		
-		void rowMult(int row, float fac) {
+		void rowMult(int* arr, int row, float fac) {
 			for(int i = 0; i < matDims; i++) {
 				arr[row][i] *= fac;
 			}
 		}
 		
-		void rowAdd(int row1, int row2, float fac) {
+		void rowAdd(int* arr, int row1, int row2, float fac) {
 			for(int i = 0; i < matDims; i++) {
 				arr[row2][i] += arr[row1] * fac;
 			}
@@ -30,13 +42,21 @@ namespace MatrixMath {
 		
 		void gaussElim() {
 			for(int i = 0; i < matDims; i++) {
-				rowMult(i, pow(arr[i][i], -1));
+				rowMult(arr, i, pow(arr[i][i], -1));
+				rowMult(identMat, i, pow(arr[i][i], -1));
 				for(int j = 0; j < matDims; j++) {
-					rowAdd(j, i, -arr[j][i]);
+					rowAdd(arr, j, i, -arr[j][i]);
+					rowAdd(identMat, j, i, -arr[j][i]);
 				}
 			}
 		}
 		
+		void outputInverse(int* output) {
+			for(int i = 0; i < matSize; i++) {
+				for(int j = 0; j < matSize; j++) {
+					output[i][j] = identMat[i][j];
+				}
+			}
+		}
 	}
-	
 }
